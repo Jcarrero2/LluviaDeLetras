b54a9fcb27cf3a00dbfae420bd60d28a4465dbca
AbelTomas <34541232+AbelTomas@users.noreply.github.com>
2018-01-26 09:16:56 +0100

    Version Base
    Creado el proyecto y las clases Controlador y Vista

---
+++ src/lluviadeletras/Controlador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lluviadeletras;
+
+/**
+ *
+ * @author Abel y Jose
+ */
+public class Controlador {
+    private Vista v;
+    public Controlador() {
+        v=new Vista();
+    }
+    
+}
